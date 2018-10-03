df0b2d6c4a2dd81cd9e7ec254c1b002f294b16b1
Grace Choi <oiograceoio@gmail.com>
2018-10-03 02:24:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 def find_the_cheese(snacks) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.find_all {|x| x == cheese_types}
+  snacks.detect {|x| x == cheese_types}
 end
